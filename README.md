5dc35f79ce2ab20d26253013d7d347dfc5864726
kaizoku-oh <garssallaoui.bayrem@gmail.com>
2021-05-13 02:54:22 +0100

    update component and example

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # pio-rfid
 RFID example using ESP32 and PlatformIO esp-idf framework
+
+## How to use?
+### Window (Powershell)
+``` powershell
+# Set local environment variables
+> $env:WIFI_SSID = '"TYPE_YOUR_WIFI_SSID_HERE"'
+> $env:WIFI_PASS = '"TYPE_YOUR_WIFI_PASSWORD_HERE"'
+> $env:FIRESTORE_FIREBASE_PROJECT_ID = '"TYPE_YOUR_FIREBASE_PROJECT_ID_HERE"'
+> $env:FIRESTORE_FIREBASE_API_KEY = '"TYPE_YOUR_FIREBASE_API_KEY_HERE"'
+
+# OPTIONAL: To read and verify the values of the variables that you just set:
+> Get-ChildItem Env:WIFI_SSID
+> Get-ChildItem Env:WIFI_PASS
+> Get-ChildItem Env:FIRESTORE_FIREBASE_PROJECT_ID
+> Get-ChildItem Env:FIRESTORE_FIREBASE_API_KEY
+```
+
+### Linux (Bash)
+``` bash
+# Set local environment variables
+$ export WIFI_SSID='"TYPE_YOUR_WIFI_SSID_HERE"'
+$ export WIFI_PASS='"TYPE_YOUR_WIFI_PASS_HERE"'
+$ export FIRESTORE_FIREBASE_PROJECT_ID='"TYPE_YOUR_FIREBASE_PROJECT_ID_HERE"'
+$ export FIRESTORE_FIREBASE_API_KEY='"TYPE_YOUR_FIREBASE_PROJECT_ID_HERE"'
+
+# OPTIONAL: To read and verify the values of the variables that you just set:
+$ echo $WIFI_SSID
+$ echo $WIFI_PASS
+$ echo $FIRESTORE_FIREBASE_PROJECT_ID
+$ echo $FIRESTORE_FIREBASE_API_KEY
+```
--- src/main.c
+++ src/main.c
@@ -20,7 +20,9 @@ static void _firestore_task(void *);
 #define ESP32_SPI_SCK_PIN                        18
 #define ESP32_SPI_SDA_PIN                        21
 
-#define DOC_MAX_SIZE                             64
+#define SERIAL_NUMBER_MAX_SIZE                   5
+
+#define FIRESTORE_DOC_MAX_SIZE                   64
 #define FIRESTORE_COLLECTION_ID                  "devices"
 #define FIRESTORE_DOCUMENT_ID                    "rfid-node"
 #define FIRESTORE_DOCUMENT_EXAMPLE               "{"                                 \
@@ -39,8 +41,8 @@ typedef enum
 
 static QueueHandle_t stQueue;
 static uint32_t u32DocLength;
-static char tcDoc[DOC_MAX_SIZE];
-static uint8_t *pu08SerialNumber;
+static char tcDoc[FIRESTORE_DOC_MAX_SIZE];
+static uint8_t tcu08SerialNumber[SERIAL_NUMBER_MAX_SIZE];
 
 static const rc522_start_args_t stStartArgs =
 {
@@ -51,20 +53,27 @@ static const rc522_start_args_t stStartArgs =
   .callback = &_tag_handler,
 };
 
+static const uint8_t ttu08KnownSerialNumbers[3][5] =
+{
+  {0x72, 0xEA, 0x5F, 0x06, 0xC1},
+  {0x29, 0x57, 0x8C, 0xBB, 0x49},
+  {0x76, 0x9E, 0x25, 0xF8, 0x35},
+};
+
 void app_main(void)
 {
   wifi_initialise();
   wifi_wait_connected();
 
-  stQueue = xQueueCreate(5, sizeof(rc522_event_t));
-  xTaskCreate(_firestore_task, "firestore", 10240, NULL, 5, NULL);
+  stQueue = xQueueCreate(10, sizeof(rc522_event_t));
+  xTaskCreate(_firestore_task, "firestore", 10240, NULL, 4, NULL);
 }
 
 static void _tag_handler(uint8_t *pu08SN)
 {
   rc522_event_t eEvent;
 
-  pu08SerialNumber = pu08SN;
+  memcpy(tcu08SerialNumber, pu08SN, sizeof(tcu08SerialNumber));
   eEvent = TAG_DETECTED_EVENT;
   xQueueSend(stQueue, &eEvent, portMAX_DELAY);
 }
@@ -83,25 +92,40 @@ static void _firestore_task(void *pvParameter)
       {
       case TAG_DETECTED_EVENT:
         ESP_LOGI(TAG,
-                 "Detected Tag with serial-number: 0x%X 0x%X 0x%X 0x%X 0x%X",
-                 pu08SerialNumber[0],
-                 pu08SerialNumber[1],
-                 pu08SerialNumber[2],
-                 pu08SerialNumber[3],
-                 pu08SerialNumber[4]);
-        _send_data(pu08SerialNumber);
+                 "Detected Tag with serial-number: %X%X%X%X%X",
+                 tcu08SerialNumber[0],
+                 tcu08SerialNumber[1],
+                 tcu08SerialNumber[2],
+                 tcu08SerialNumber[3],
+                 tcu08SerialNumber[4]);
+        if((0 == memcmp(tcu08SerialNumber, ttu08KnownSerialNumbers[0], sizeof(tcu08SerialNumber))) ||
+           (0 == memcmp(tcu08SerialNumber, ttu08KnownSerialNumbers[1], sizeof(tcu08SerialNumber))) ||
+           (0 == memcmp(tcu08SerialNumber, ttu08KnownSerialNumbers[2], sizeof(tcu08SerialNumber))))
+        {
+          ESP_LOGI(TAG, "Tag is recognized");
+        }
+        else
+        {
+          ESP_LOGW(TAG, "Tag is not recognized");
+        }
+        /* Sending data anyway */
+        _send_data(tcu08SerialNumber);
         break;
       default:
         ESP_LOGW(TAG, "Unknow event");
         break;
       }
     }
+    else
+    {
+      ESP_LOGE(TAG, "Couldn't receive item from queue");
+    }
   }
 }
 
 static void _send_data(uint8_t *pu08SN)
 {
-  /* Format document with newly fetched data */
+  /* Format json document */
   u32DocLength = snprintf(tcDoc,
                           sizeof(tcDoc),
                           "{\"fields\":{\"sn\":{\"stringValue\":\"%X%X%X%X%X\"}}}",
@@ -111,17 +135,18 @@ static void _send_data(uint8_t *pu08SN)
                           pu08SN[3],
                           pu08SN[4]);
   ESP_LOGI(TAG, "Document length after formatting: %d", u32DocLength);
+  ESP_LOGI(TAG, "Document content after formatting:\r\n%.*s", u32DocLength, tcDoc);
   if(u32DocLength > 0)
   {
     /* Update document in firestore or create it if it doesn't already exists */
-    if(FIRESTORE_OK == firestore_update_document("devices",
-                                                 "rfid-node",
+    if(FIRESTORE_OK == firestore_update_document(FIRESTORE_COLLECTION_ID,
+                                                 FIRESTORE_DOCUMENT_ID,
                                                  tcDoc,
                                                  &u32DocLength))
     {
       ESP_LOGI(TAG, "Document updated successfully");
-      ESP_LOGD(TAG, "document length: %d", u32DocLength);
-      ESP_LOGD(TAG, "document content:\r\n%.*s", u32DocLength, tcDoc);
+      ESP_LOGD(TAG, "Document length: %d", u32DocLength);
+      ESP_LOGD(TAG, "Document content:\r\n%.*s", u32DocLength, tcDoc);
     }
     else
     {
